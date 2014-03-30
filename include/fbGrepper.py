9cf73498d343a8345eb43005c896cd265398349a
Ryan2013 <b9807014@mail.ntust.edu.tw>
2014-03-30 23:42:47 +0800

    add get friend likes in corresponding status but note that the number of limitation of likes is 25...

--- include/fbGrepper.py
+++ include/fbGrepper.py
@@ -1,16 +1,20 @@
 import facebook
 import sys
+def enum(**enums):
+    return type('Enum', (), enums)
+
+FRIEND_INFO = enum(ABOUT = 'about', STATUSES = 'statuses')
 
 def get_my_friends(graph):
     print "get friends...."
-    friendlist = graph.get_connections('me','friends')['data']
+    friendlist = graph.get_connections('me', 'friends')['data']
     friends = [(friend['id'], friend['name'],) for friend in friendlist]
     print 'total of friend = %d\n' %len(friends)
     return friends
 
 def get_my_feed(graph):
     print "get my feed...."
-    feed_dict = graph.get_connections('me', 'feed')
+    feed_dict = graph.get_connections('me', 'feed')['data']
     return feed_dict
 
 def cvtDict2Chinese(dict):
@@ -21,9 +25,38 @@ def get_my_friends_about(graph, friends):
     print "get my friend about...."
     friendlist_about = []
     total_friend = len(friends)
-    current_processing = 1
+    current_friend = 1
     for friend in friends:
-        print "%d/%d....." %(current_processing,total_friend)
+        print "%d/%d....." %(current_friend, total_friend)
         friendlist_about.append(graph.get_object(friend[0]))
-        current_processing += 1
+        current_friend += 1
     return friendlist_about
+
+#statuses is defined that you post on your own wall
+#friendlist_status save as [id, status_list corresponding to id]
+def get_my_friends_status(graph, friends):
+    print "get my friends status...."
+    friendlist_status = []
+    total_friend = len(friends)
+    current_friend = 1
+    for friend in friends:
+        print "%d/%d....." %(current_friend, total_friend)
+        id = friend[0]
+        friendlist_status.append((id, graph.get_connections(id, 'statuses')['data']))
+        current_friend += 1
+    return friendlist_status
+
+def get_likes(friendlist_status):
+    print "get likes info about statuses...."
+    friendlist_like = []
+    total_statuses = len(friendlist_status)
+    current_status = 1
+    for s in friendlist_status:
+        print "%d/%d...." %(current_status, total_statuses)
+        status = s[1]
+        for post in status:
+            if 'message' in post and 'likes' in post:
+                #save nessage for testing,and consider whether the corresponding comment need to be saved
+                friendlist_like.append((post['message'], post['likes']))
+        current_status += 1
+    return friendlist_like
--- src/mining.py
+++ src/mining.py
@@ -6,19 +6,23 @@ lib_path = os.path.abspath('./../include/')
 sys.path.append(lib_path)
 from fbGrepper import *
 
-auth_access_token = "CAACEdEose0cBAFIXvicm4FDIwjxFticxgZAFD6QW3yQpgN90ZASKwel5w78ZCEbmGlM7yyEXva1BDNHlaFjoMAEC8Ll3SSyyXyZAljTqwrC0KT8n94OfP9zWNMJhA7ZAxvE2tjMN2L6ZAGDVOv7wbxViMqr8agNQ8cCWaxUQK4FtCS1mIs2q7uCqflS41vNlEZD"
+auth_access_token = "CAACEdEose0cBAAiniVjzuSvc3nN24ZAicYuVMTLADiZB1S5fYPqBe24XqSk0BEbndg2lF9AxseuFD2tjraLJOoBCZATledOvIMDqbeFfSNCnPF9qtnBT70FRv371mUPIhb6UZC8ef8j8NOvitkkl4YanHXJI0qLv7GBZAPZBzn63gW3ZCSEKA8etJeE3IN9QcyYc7YcmzGEYgZDZD"
 
 def main():
     graph = facebook.GraphAPI(auth_access_token)
     friends = get_my_friends(graph)
-    print friends
-    feed = get_my_feed(graph)
-    print feed
-
-    friendlist_about = get_my_friends_about(graph, friends)
-    for about in friendlist_about:
-        if 'gender' in about:
-            print about["name"] + " " + about["gender"]
+    #print cvtDict2Chinese(friends)
+    #feed = get_my_feed(graph)
+    #print cvtDict2Chinese(feed)
+    friendlist_status = get_my_friends_status(graph, friends)
+    print cvtDict2Chinese(friendlist_status)
+    likeslist = get_likes(friendlist_status)
+    print 'likeslist'
+    print cvtDict2Chinese(likeslist)
+    #friendlist_about = get_my_friends_about(graph, friends)
+    #for about in friendlist_about:
+    #    if 'gender' in about:
+    #        print about["name"] + " " + about["gender"]
 
 if __name__ == "__main__":
     main()
