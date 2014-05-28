81428fb49d0d76639e1607fcd3ac492e1f26adb5
Ryan2013 <b9807014@mail.ntust.edu.tw>
2014-05-28 21:53:50 +0800

    NEW_FEATURE:load the data from the new format which is converted from csvtodata.py features: 1. load the new formated data 2. fix some bugs

---
+++ src/base.py
@@ -0,0 +1,37 @@
+import csv
+
+import numpy as np
+class Bunch(dict):
+
+    def __init__(self, **kwargs):
+        dict.__init__(self, kwargs)
+        self.__dict__ = self
+
+def load_like_comment():
+    """""Load and return the heterosexual's all likes and comments corresponding to author
+
+    ==========================================================
+    ||     data        ||  source ||    target       ||
+       likes   comments   hex_ID    author_ID  gender
+    ===========================================================
+    Examples
+    """""
+    with open('data/1.csv') as csv_file:
+        data_file = csv.reader(csv_file)
+        temp = next(data_file)
+        n_hex = int(temp[0])
+        n_features = int(temp[1])
+        target_id = int(temp[2])
+        target_gender = temp[3]
+        data = np.empty((n_hex, n_features))
+        hex_id = np.empty((n_hex,), dtype=np.int)
+
+        for i, info in enumerate(data_file):
+            hex_id[i] = np.asarray(info[0], dtype=np.int)
+            data[i] = np.asarray(info[1:], dtype=np.int)
+    return Bunch(data=data, hex_id=hex_id,
+                 target_id=target_id,
+                 target_gender=target_gender)
+
+
+    
--- src/csvtodata.py
+++ src/csvtodata.py
@@ -11,7 +11,7 @@ for ls_cmd in sys.stdin:
     file_content = []
     data_file = csv.reader(csv_file)
     temp = next(data_file)
-    target_id = temp[0][10:-1]
+    target_id = temp[0][10:]
     target_gender = temp[2]
     n_hex = 0
     n_features = 2
@@ -21,8 +21,8 @@ for ls_cmd in sys.stdin:
             file_content.append(hex_info)
             n_hex = n_hex + 1
 
-    ofile.write(''.join(str(n_hex) + ' ' + str(n_features) + ' ' + target_id + ' ' + target_gender + '\n'))
-    for i in range(1, n_hex):
-        ofile.write(''.join(file_content[i][0] + ' ' + file_content[i][2] + ' ' + file_content[i][2+int(file_content[i][2])+2]) + '\n')
+    ofile.write(''.join(str(n_hex) + ',' + str(n_features) + ',' + target_id + ',' + target_gender + '\n'))
+    for i in range(0, n_hex):
+        ofile.write(''.join(file_content[i][0] + ',' + file_content[i][2] + ',' + file_content[i][2+int(file_content[i][2])+2]) + '\n')
     csv_file.close()
     ofile.close()
